0c1310c9f314acea53b077cdec0f7b1e7b522592
ellieisjelly <ellie@ellieis.me>
2024-02-15 20:48:01 -0300

    funny things

--- src/components/Server.tsx
+++ src/components/Server.tsx
@@ -4,6 +4,7 @@ import versionImg from "../assets/code-branch-solid.svg";
 import tpsImg from "../assets/microchip-solid.svg";
 import customModeImg from "../assets/gamepad-solid.svg";
 import { Show } from "solid-js";
+import { server } from "../pages/ServerList";
 function getGameTypeString(gameType: number) {
   return gameType == 4
     ? "World"
@@ -22,101 +23,107 @@ function getGameTypeString(gameType: number) {
 function getMapFromGameType(gameType: number) {
   return gameType == 4 ? "test2" : "Round";
 }
-export default function Server(props: {
-  id: number;
-  name: string;
-  address: string;
-  port: number;
-  version: number;
-  gameType: number;
-  players: number;
-  maxPlayers: number;
-  build: string;
-  tps?: number;
-  mode?: { name: string };
-  map?: string;
-}) {
+export default function Server(props: { server: server }) {
   return (
     <div class="bg-crust py-1 border-y-[1px] border-crust rounded-2xl">
-      <div class="px-2">
-        <h1 class="text-2xl font-bold glow-small">{props.name}</h1>
-        <div class="flex flex-row gap-4">
-          <div class="flex flex-row">
-            <img
-              src={playerImg}
-              class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-              alt="Players:"
-            ></img>
-            <h2 class="text-sm sm:px-2 sm:text-lg">
-              {props.players}/{props.maxPlayers}
-            </h2>
-          </div>
-          <Show when={props.mode}>
-            <div class="flex flex-row">
+      <a href={props.server.link}>
+        <div class="px-2">
+          <h1 class="text-2xl font-bold glow-small block">
+            <Show when={props.server.icon}>
               <img
-                src={customModeImg}
-                class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-                alt="Game Mode:"
+                src={props.server.icon}
+                class="aspect-square w-auto h-8 rounded-full mr-2 inline"
               ></img>
-              <h2 class="text-sm sm:px-2 sm:text-lg">{props.mode?.name}</h2>
+            </Show>
+            {props.server.name}
+          </h1>
+          <div class={"flex flex-row gap-4"}>
+            <div class={"flex flex-row gap-x-4"}>
+              <div class="flex flex-row">
+                <img
+                  src={playerImg}
+                  class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                  alt="Players:"
+                ></img>
+                <h2 class="text-sm sm:px-2 sm:text-lg">
+                  {props.server.players}/{props.server.maxPlayers}
+                </h2>
+              </div>
+              <Show when={props.server.customMode}>
+                <div class="flex flex-row">
+                  <img
+                    src={customModeImg}
+                    class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                    alt="Game Mode:"
+                  ></img>
+                  <h2 class="text-sm sm:px-2 sm:text-lg">
+                    {props.server.customMode?.name}
+                  </h2>
+                </div>
+                <div class="flex flex-row">
+                  <img
+                    src={gamemodeImg}
+                    class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                    alt="Game Mode:"
+                  ></img>
+                  <h2 class="text-sm sm:px-2 sm:text-lg">
+                    {props.server.map?.toLowerCase()}
+                  </h2>
+                </div>
+              </Show>
+              <Show when={!props.server.customMode}>
+                <div class="flex flex-row">
+                  <img
+                    src={customModeImg}
+                    class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                    alt="Game Mode:"
+                  ></img>
+                  <h2 class="text-sm sm:px-2 sm:text-lg">
+                    {getGameTypeString(props.server.gameType)}
+                  </h2>
+                </div>
+                <div class="flex flex-row">
+                  <img
+                    src={gamemodeImg}
+                    class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                    alt="Game Mode:"
+                  ></img>
+                  <h2 class="text-sm sm:px-2 sm:text-lg">
+                    {getMapFromGameType(props.server.gameType).toLowerCase()}
+                  </h2>
+                </div>
+              </Show>
+              <Show when={props.server.tps}>
+                <div class="flex flex-row">
+                  <img
+                    src={tpsImg}
+                    class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
+                    alt=""
+                  ></img>
+                  <h2 class="text-sm sm:px-2 sm:text-lg">
+                    {props.server.tps?.toFixed(2)} TPS
+                  </h2>
+                </div>
+              </Show>
+              <div class="flex flex-row">
+                <img
+                  src={versionImg}
+                  height="16"
+                  width="16"
+                  alt="Version:"
+                ></img>
+                <h2
+                  class="text-sm sm:px-2 sm:text-lg"
+                  style={`color: hsl(${props.server.version * 6}, 100%, 60%)`}
+                >
+                  {props.server.version}
+                  {props.server.build}
+                </h2>
+              </div>
             </div>
-            <div class="flex flex-row">
-              <img
-                src={gamemodeImg}
-                class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-                alt="Game Mode:"
-              ></img>
-              <h2 class="text-sm sm:px-2 sm:text-lg">
-                {props.map?.toLowerCase()}
-              </h2>
-            </div>
-          </Show>
-          <Show when={!props.mode}>
-            <div class="flex flex-row">
-              <img
-                src={customModeImg}
-                class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-                alt="Game Mode:"
-              ></img>
-              <h2 class="text-sm sm:px-2 sm:text-lg">
-                {getGameTypeString(props.gameType)}
-              </h2>
-            </div>
-            <div class="flex flex-row">
-              <img
-                src={gamemodeImg}
-                class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-                alt="Game Mode:"
-              ></img>
-              <h2 class="text-sm sm:px-2 sm:text-lg">
-                {getMapFromGameType(props.gameType).toLowerCase()}
-              </h2>
-            </div>
-          </Show>
-          <Show when={props.tps}>
-            <div class="flex flex-row">
-              <img
-                src={tpsImg}
-                class="w-[18px] h-[18px] sm:w-[24px] sm:h-[24px]"
-                alt=""
-              ></img>
-              <h2 class="text-sm sm:px-2 sm:text-lg">
-                {props.tps?.toFixed(2)} TPS
-              </h2>
-            </div>
-          </Show>
-          <div class="flex flex-row">
-            <img src={versionImg} height="16" width="16" alt="Version:"></img>
-            <h2
-              class="text-sm sm:px-2 sm:text-lg"
-              style={`color: hsl(${props.version * 6}, 100%, 60%)`}
-            >
-              {props.version}
-              {props.build}
-            </h2>
           </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
--- src/pages/ServerList.tsx
+++ src/pages/ServerList.tsx
@@ -16,35 +16,26 @@ type server = {
   tps?: number;
   customMode?: { name: string };
   map: string;
+  icon?: string;
+  link?: string;
+  description?: string;
 };
 const [servers] = createResource(async () => {
   const serverList = await (await fetch(`${ApiURL}/servers`)).json();
   serverList.sort((a: server, b: server) => {
     return a.players > b.players ? -1 : 1;
   });
+  console.log(serverList);
   return serverList;
 });
+
+export type { server };
 export default function ServerList() {
   return (
     <div>
       <div class="grid grid-cols-1 lg:grid-cols-2 3xl:grid-cols-3 mx-2 sm:mx-8 my-8 gap-2">
         <For each={servers()}>
-          {(server: server) => (
-            <Server
-              id={server.id}
-              name={server.name}
-              address={server.address}
-              port={server.port}
-              version={server.version}
-              gameType={server.gameType}
-              players={server.players}
-              maxPlayers={server.maxPlayers}
-              build={server.build}
-              tps={server.tps}
-              mode={server.customMode}
-              map={server.map}
-            ></Server>
-          )}
+          {(server: server) => <Server server={server}></Server>}
         </For>
       </div>
       <div class="my-8 mx-2 flex justify-center">
